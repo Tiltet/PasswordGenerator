a6152ec349945612b389133be2367906c9cf39ab
Tiltet <timofeysavinich@mail.ru>
2024-02-21 11:40:27 +0300

    feat: add DAO struct

--- src/main/java/labs/passwordgenerator/model/PasswordGenerator.java
+++ src/main/java/labs/passwordgenerator/model/PasswordGenerator.java
@@ -1,11 +1,12 @@
 package labs.passwordgenerator.model;
 
-
 import lombok.Builder;
 import lombok.Data;
+import lombok.extern.jackson.Jacksonized;
 
 @Data
 @Builder
+@Jacksonized
 public class PasswordGenerator {
-
+    private String random_password;
 }
